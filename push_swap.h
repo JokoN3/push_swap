be4d7434c3735669917109c31c2d2a0e37aab086
JokoN3 <jokosan97@gmail.com>
2026-04-09 18:59:54 +0200

    added struct

--- push_swap.h
+++ push_swap.h
@@ -6,11 +6,19 @@
 /*   By: yoneshev <yoneshev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/09 18:13:52 by yoneshev      #+#    #+#                 */
-/*   Updated: 2026/04/09 18:14:10 by yoneshev      ########   odam.nl         */
+/*   Updated: 2026/04/09 18:55:47 by yoneshev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include "limits.h"
+
+typedef struct s_stack
+{
+	int				num;
+	typedef struct	s_stack *next;
+}	t_stack;
 
 #endif
