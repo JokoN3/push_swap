9c4af21a1da046f0ecfa3c4062138f6e322aeeaa
Linde Van Wingerden <lvan-win@f1r2s15.codam.nl>
2026-04-15 17:44:30 +0200

    start disorder

--- disorder.c
+++ disorder.c
@@ -6,20 +6,31 @@
 /*   By: lvan-win <lvan-win@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/15 16:26:05 by lvan-win      #+#    #+#                 */
-/*   Updated: 2026/04/15 17:02:14 by lvan-win      ########   odam.nl         */
+/*   Updated: 2026/04/15 17:44:08 by lvan-win      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-float	compute_disorder(t_stack a)
+float	compute_disorder(t_stack *a)
 {
-	int	mistakes;
-	int	total_pairs;
-	int	i;
-	int	j;
+	int		mistakes;
+	int		total_pairs;
+	t_stack	*current;
+	t_stack	*next;
 
 	mistakes = 0;
 	total_pairs = 0;
-	i = 0;
+	current = a;
+	while (i < stack_len(a)) //dit met linked lists maken, later fixen
+	{
+		j = i + 1;
+		while (j < stack_len(a))
+		{
+			total_pairs++;
+			if ()
+		}
+	}
 }
+
+// with two decimals
